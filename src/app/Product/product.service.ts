148c5a81769354369c0f6494cbe4635ab5436456
aziz-kck <61433292+aziz-kck@users.noreply.github.com>
2023-05-06 16:18:56 +0100

    push3

---
+++ src/app/Product/product.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Product} from "./product";
+import {Attribute} from "../product-details/attribute";
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  public host:string = "http://localhost:8080/Product";
+
+  constructor(private http:HttpClient) { }
+
+  public getProduct():Observable<Product[]>{
+    return this.http.get<Product[]>(this.host+"/showAll");
+  }
+  public getProductById(id: number): Observable<Product>{
+    return this.http.get<Product>(this.host+"/showProdById/"+id);
+  }
+  public createProduct(product:Product):Observable<Object>{
+    return this.http.post(this.host+"/addprod",product);
+  }
+
+  public updateProduct(product:Product): Observable<Object>{
+    return this.http.put(this.host+"/updateprod",product);
+  }
+
+  public DeleteProduct(id: number): Observable<Object>{
+    return this.http.delete(this.host+"/deleteProdbyID/"+id);
+
+  }
+
+public showAttribute():Observable<Attribute[]>{
+  return this.http.get<Attribute[]>(this.host+"/attribute");
+}
+public AfficherAttributByID(id: number):Observable<Attribute>{
+    return this.http.get<Attribute>(this.host+"/showAttributeById/"+id);
+}
+}
